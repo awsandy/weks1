6526d6166963801bb04bb5e393883dd203266a28
awsandy <awsandy@amazon.co.uk>
2020-06-06 12:41:00 +0100

    setup minus keys

--- scripts/setup-tools.sh
+++ scripts/setup-tools.sh
@@ -27,16 +27,16 @@ kubectl completion bash >>  ~/.bash_completion
 . /etc/profile.d/bash_completion.sh
 . ~/.bash_completion
 
-echo "ssh key"
-mkdir -p ~/.ssh
-ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
-chmod 600 ~/.ssh/id*
-aws ec2 delete-key-pair --key-name "eksworkshop"
-aws ec2 import-key-pair --key-name "eksworkshop" --public-key-material fileb://~/.ssh/id_rsa.pub
-echo "KMS key"
-aws kms create-alias --alias-name alias/eksworkshop --target-key-id $(aws kms create-key --query KeyMetadata.Arn --output text)
-export MASTER_ARN=$(aws kms describe-key --key-id alias/eksworkshop --query KeyMetadata.Arn --output text)
-echo "export MASTER_ARN=${MASTER_ARN}" | tee -a ~/.bash_profile
+#echo "ssh key"
+#mkdir -p ~/.ssh
+#ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
+#chmod 600 ~/.ssh/id*
+#aws ec2 delete-key-pair --key-name "eksworkshop"
+#aws ec2 import-key-pair --key-name "eksworkshop" --public-key-material fileb://~/.ssh/id_rsa.pub
+#echo "KMS key"
+#aws kms create-alias --alias-name alias/eksworkshop --target-key-id $(aws kms create-key --query KeyMetadata.Arn --output text)
+#export MASTER_ARN=$(aws kms describe-key --key-id alias/eksworkshop --query KeyMetadata.Arn --output text)
+#echo "export MASTER_ARN=${MASTER_ARN}" | tee -a ~/.bash_profile
 
 echo "install eksctl"
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
